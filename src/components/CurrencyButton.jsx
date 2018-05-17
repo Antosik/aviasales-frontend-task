28fd9e4c2efbbe9f7d71e14cbdb044098068c376
Antosik <grig_anton96@mail.ru>
2018-05-17 19:30:31 +0300

    [add][task4] CurrencyButton style

---
+++ src/components/CurrencyButton.jsx
@@ -0,0 +1,60 @@
+import React from "react";
+import styled from "styled-components";
+
+const CurrencyButtonText = styled.div`
+  position: relative;
+  border: 1px solid #d2d5d6;
+  background: #ffffff;
+  color: #2196f3;
+  text-align: center;
+  padding: 12px 8px;
+  cursor: pointer;
+  transition: background 150ms ease-in-out;
+`;
+
+const CurrencyButtonInput = styled.input.attrs({
+  type: "radio"
+})`
+  position: absolute;
+  clip: rect(0 0 0 0);
+  width: 1px;
+  height: 1px;
+  margin: -1px;
+
+  &:checked + ${CurrencyButtonText}, &:checked:active + ${CurrencyButtonText} {
+    border: 1px solid #2196f3;
+    background: #2196f3;
+    color: #fff;
+  }
+
+  &:active + ${CurrencyButtonText} {
+    background: #f2fcff;
+    border: 1px solid #63b5f5;
+    color: #2196f3;
+  }
+
+  &:checked:active + ${CurrencyButtonText} {
+
+  }
+`;
+
+const CurrencyButtonContainer = styled.label`
+  flex: 1;
+
+  &:first-child ${CurrencyButtonText} {
+    border-top-left-radius: 8px;
+    border-bottom-left-radius: 8px;
+  }
+
+  &:last-child ${CurrencyButtonText} {
+    border-top-right-radius: 8px;
+    border-bottom-right-radius: 8px;
+  }
+`;
+
+export default ({ name, children }) => (
+  <CurrencyButtonContainer>
+    <CurrencyButtonInput name={name} />
+    <CurrencyButtonText>{children}</CurrencyButtonText>
+  </CurrencyButtonContainer>
+);
