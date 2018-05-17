484c2a797ec5927cd13231aeadff8b7dfac90da3
Antosik <grig_anton96@mail.ru>
2018-05-17 18:16:32 +0300

    [init] index files

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "core-js/es6/map";
+import "core-js/es6/set";
+
+import React from "react";
+import ReactDOM from "react-dom";
+
+import App from "./app";
+
+ReactDOM.render(<App />, document.getElementById("app"));
